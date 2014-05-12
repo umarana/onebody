8c20ba538d21402b6817c9d0ad105920bcc03c5c
Tim Morgan <tim@timmorgan.org>
2014-05-11 22:55:03 -0500

    Started work on upgrading to Rails 4.1.
    Unfortunately, #count and #any? methods on relations where there is a
    custom select are failing on Rails 4.1. See here for more info:
    
    https://github.com/rails/rails/issues/13648

--- config/initializers/verify_secrets.rb
+++ config/initializers/verify_secrets.rb
@@ -1,7 +1,4 @@
 secrets = YAML.load_file(Rails.root.join('config/secrets.yml'))
-token = secrets[Rails.env]['secret_token']
-if token == 'SOMETHING_RANDOM_HERE'
+if secrets[Rails.env]['secret_key_base'] == 'SOMETHING_RANDOM_HERE'
   raise StandardError.new('You forgot to set the secret token in config/secrets.yml')
-else
-  OneBody::Application.config.secret_key_base = token
 end
